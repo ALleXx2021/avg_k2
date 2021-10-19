b72576ac97364d76b59be6ba4efaaf52158dce21
izaplachta <izaplachta@gmail.com>
2021-10-19 11:51:34 +0200

    Implementacja funkcji srednia

--- avg.php
+++ avg.php
@@ -1,17 +1,9 @@
 <?php
-
-	function srednia(int a, int b)
-	{
-		return (a+b)/2;
-	}
+// pamietaj o zapisaniu pliku ctrl + s
+function srednia(int a, inb b)
+{
+	return (a+b)/2;
+}
 	
-	function wsrednia (int a, int b)
-	{
-<<<<<<< HEAD
-		return (6*a+4*b)/10;
-=======
-		return (8*a+2*b)/10;
->>>>>>> konflikt
-	}
 	
 ?>
